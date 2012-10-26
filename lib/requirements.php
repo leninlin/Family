1931a0905cc267203b329a2889f1964f0df4181d
Виктория <sirena0790@sirena0790-dell.(none)>
2012-10-26 09:24:26 +0400

    clean

--- lib/requirements.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-if (version_compare(PHP_VERSION, '5.3.4') < 0) die('<span style="color:#B22222;">Version of your PHP is ' . PHP_VERSION . ', but Aleph requires not less than PHP 5.3.4</span>');
-echo 'All requirements are met.';
-
-?>
